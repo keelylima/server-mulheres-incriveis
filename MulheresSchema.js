e94c1fb652c60ba1f598d0f9d2ac8dc39069cd30
Kelly Christina <keely06@live.com>
2019-06-14 13:58:58 -0300

    conectar banco com mongoose

--- MulheresSchema.js
+++ MulheresSchema.js
@@ -3,11 +3,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const MulheresSchema = new Schema({
     _id: {type: mongoose.Schema.Types.Object, auto: true},
-    nome: {type: String, required: true},
-    profissao: {type: String, required: true},
-    descricao: {type: String, required: true},
-    perfil: {type: String, required: true},
-    img_art_01: {type: String, required: true},
-    img_art_02: {type: String, required: true},
-    img_art_03: {type: String, required: true}
+    nome: {type: String, required: true}, 
+    profissao: {type: String, }, //não esquecer de colocar require em todos
+    descricao: {type: String, },
+    perfil: {type: String, },
+    img_art_01: {type: String, },
+    img_art_02: {type: String, },
+    img_art_03: {type: String, }
 })
+
+const mulheresModel = mongoose.model('mulheres-incriveis', MulheresSchema);
+
+module.exports = { mulheresModel };
