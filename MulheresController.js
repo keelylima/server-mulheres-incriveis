0f92b588ddf22954c480ebd7cf41ac65e5784c8c
Kelly Christina <keely06@live.com>
2019-06-14 13:59:26 -0300

    metodo get

--- MulheresController.js
+++ MulheresController.js
@@ -1,1 +1,17 @@
-s
+const { connect } = require('./MulheresRepository');
+const { mulheresModel } = require('./MulheresSchema');
+connect()
+
+const getAll = async () => {
+    return mulheresModel.find((error, mulheres) => {
+        if(error) {
+            console.error(error);
+        }
+        return mulheres;
+    });
+}
+
+
+module.exports = {
+    getAll
+}
